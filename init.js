49b4af2cfecf7b7f44ce7e58622e36888a4bc4fd
Zin <ethan@yaacobi.co.in>
2024-03-05 19:53:50 -0500

    adwagag

--- init.js
+++ init.js
@@ -2,37 +2,16 @@ const userid = config.userid;
 let acts = [];
 
 const getPfpUrl = (id, discrim, avatar) => {
-    if(avatar && avatar !== null) {
+    if (avatar && avatar !== null) {
         return `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`;
     }
-    if(discrim && discrim !== null && discrim !== "0") {
+    if (discrim && discrim !== null && discrim !== "0") {
         return `https://cdn.discordapp.com/embed/avatars/${parseInt(discrim) % 5}.png`;
     }
     return `https://cdn.discordapp.com/embed/avatars/${id.substr(id.length - 1)}.png`;
 }
 
-let title = "loading...";
-let titlei = 1;
-let wait = 0;
-
-setInterval(() => {
-  document.title = title.substring(0, titlei);
-
-  if(titlei == title.length) {
-    if(wait == 3) {
-      titlei = 1;
-      wait = 0;
-    }
-    wait++;
-  } else {
-    titlei++;
-  }
-}, 400)
-
 const load = () => {
-    // backdrop from config
-    const root = document.querySelector(":root");
-    root.style.setProperty("--backdrop-url", `url("${config.profile_backdrop_url}")`);
     // init avatar in case it doesn't work
     const avaElem = document.getElementById("avatar");
     avaElem.src = getPfpUrl(userid, undefined, undefined);
@@ -40,7 +19,7 @@ const load = () => {
     const viewElem = document.getElementById("viewuser");
     viewElem.href = `https://discord.com/users/${userid}`;
     console.log("Page init");
-    
+
     lanyard({
         userId: userid,
         socket: true,
@@ -56,7 +35,7 @@ const update = data => {
 }
 
 const draw = data => {
-    if(title === "loading...") title = `${data.discord_user.username}'s discord status`; 
+    if (title === "loading...") title = `${data.discord_user.username}'s discord status`;
     const displayNameElem = document.getElementById("display");
     displayNameElem.innerText = data.discord_user.global_name;
 
@@ -64,26 +43,26 @@ const draw = data => {
     nameElem.innerText = data.discord_user.username;
 
     const statusElem = document.getElementById("status");
-    
+
     const avaElem = document.getElementById("avatar");
     avaElem.src = getPfpUrl(data.discord_user.id, data.discord_user.discrim, data.discord_user.avatar);
-    
-    const userinfo = document.getElementById("margin");
+
+    const userinfo = document.getElementById("user-info");
     const actsWrapper = document.querySelector(".activities");
     let toRemove = [];
     actsWrapper.childNodes.forEach(node => {
-        if(acts.filter(act => act.id == node).length === 0) toRemove.push(node);
+        if (acts.filter(act => act.id == node).length === 0) toRemove.push(node);
     });
     toRemove.forEach(node => actsWrapper.removeChild(node));
 
     acts.forEach(act => {
 
-        if(act.type === 4) {
-            if(act.state && act.emoji) {
+        if (act.type === 4) {
+            if (act.state && act.emoji) {
                 statusElem.innerText = `${act.emoji.name} ${act.state}`;
-            } else if(!act.emoji) {
+            } else if (!act.emoji) {
                 statusElem.innerText = `${act.state}`;
-            } else if(!act.state) {
+            } else if (!act.state) {
                 statusElem.innerText = `${act.emoji.name}`;
             } else {
                 statusElem.innerText = "";
@@ -91,109 +70,42 @@ const draw = data => {
         } else {
             const isSpotify = act.id === "spotify:1" && data.spotify;
 
-            // <div class="activity" id="00">
+            // create activity element
             const actElem = document.createElement("div");
             actElem.classList.add("activity");
-            actElem.id = act.id;
 
-            // <div class="act-images">
-            const actImageWrapper = document.createElement("act-images");
-            actImageWrapper.classList.add("act-images");
-            
-            // <img class="largeimage" draggable="false" width="64" height="64" src="" />
+            // create large image element
             const largeImageElem = document.createElement("img");
             largeImageElem.classList.add("largeimage");
-            largeImageElem.draggable = false;
-            largeImageElem.width = largeImageElem.height = 64;
-            // spotify
-            if(isSpotify) {
-                largeImageElem.src = data.spotify.album_art_url;
-            } else {
-                // everything after "mp:" is the image id
-                // mp:external/ftBjuYHxeAs2FW1lMnr-_BxOSttEZIc1aAzg_W3nFlM/https/raw.githubusercontent.com/LeonardSSH/vscord/main/assets/icons/js.png
-                
-                if(act.assets) {
-                    if(act.assets.large_image) {
-                        const imageid = act.assets.large_image.substring(3);
-                        largeImageElem.src = `https://media.discordapp.net/${imageid}`;
-                    }
-                } else {
-                    // no easy way of getting the icons from the discord api
-                    largeImageElem.src = `https://dcdn.dstn.to/app-icons/${act.application_id}`;
-                }
-            }
-            actImageWrapper.appendChild(largeImageElem);
+            largeImageElem.src = isSpotify ? data.spotify.album_art_url : `https://media.discordapp.net/${act.assets.large_image.substring(3)}`;
+            actElem.appendChild(largeImageElem);
 
-            if(act.assets || isSpotify) {
-                if(act.assets.small_image || isSpotify) {
-                    // <img class="smallimage" draggable="false" width="32" height="32" src="" />
-                    const smallImageElem = document.createElement("img");
-                    smallImageElem.classList.add("smallimage");
-                    smallImageElem.draggable = false;
-                    smallImageElem.width = smallImageElem.height = 32;
-                    if(isSpotify) {
-                        smallImageElem.src = "https://developer.spotify.com/images/guidelines/design/icon3@2x.png";
-                    } else {
-                        const imageid = act.assets.small_image.substring(3);
-                        smallImageElem.src = `https://media.discordapp.net/${imageid}`;
-                    }
-                    actImageWrapper.appendChild(smallImageElem);
-                }
+            // create small image element
+            if (!isSpotify) {
+                const smallImageElem = document.createElement("img");
+                smallImageElem.classList.add("smallimage");
+                smallImageElem.src = `https://cdn.discordapp.com/app-assets/${act.application_id}/${act.assets.small_image}.png`;
+                actElem.appendChild(smallImageElem);
             }
 
-            // add class="act-images" to class="activity"
-            actElem.appendChild(actImageWrapper);
+            // create name element
+            const nameElem = document.createElement("div");
+            nameElem.classList.add("name");
+            nameElem.innerText = act.name;
+            actElem.appendChild(nameElem);
 
-            // <span class="act-name">placeholder name</span>
-            const nameSpan = document.createElement("span");
-            nameSpan.classList.add("act-name");
-            // <span class="act-details">placeholder details</span>
-            const detailsSpan = document.createElement("span");
-            detailsSpan.classList.add("act-details");
-            // <span class="act-state">placeholder state</span>
-            const stateSpan = document.createElement("span");
-            stateSpan.classList.add("act-state");
-            
-            if(isSpotify) {
-                nameSpan.innerText = `${data.spotify.song}`;
-                detailsSpan.innerText = `on ${data.spotify.album}`;
-                stateSpan.innerText = `by ${data.spotify.artist}`;
-            } else {
-                nameSpan.innerText = act.name ? act.name : "";
-                const start = act.timestamps.start;
-                const exp_time = Math.floor(Date.now() / 1000);
-                const diff = (exp_time * 1000) - start;
-                const timestamp = formatTime(diff);
-                detailsSpan.innerText = act.details ? act.details : timestamp;
-                stateSpan.innerText = act.state ? act.state : "";
-            }
-            
-            actElem.appendChild(nameSpan);
-            actElem.appendChild(detailsSpan);
-            actElem.appendChild(stateSpan);
+            // create details element
+            const detailsElem = document.createElement("div");
+            detailsElem.classList.add("details");
+            detailsElem.innerText = act.details;
+            actElem.appendChild(detailsElem);
 
-            // add class="activity" to activities
+            // add activity element to wrapper
             actsWrapper.appendChild(actElem);
         }
     });
 
     userinfo.style.marginBottom = actsWrapper.children.length === 0 ? "0" : "1em";
-}
-
-const formatTime = (ms) => {
-    const seconds = Math.floor((ms / 1000) % 60);
-    const minutes = Math.floor((ms / 1000 / 60) % 60);
-    const hours = Math.floor((ms / 1000 / 3600) % 60);
-
-    if(hours > 0) {
-        return `for ${hours} hour(s)`;
-    }
-    if(minutes > 0) {
-        return `for ${minutes} minute(s)`;
-    }
-    if(seconds > 0) {
-        return `for ${seconds} second(s)`;
-    }
 }
 
 // start the websocket to automatically fetch the new details on presence update
